c95ba56014004c9091cf1d28560d9938ceab585e
Ramling-Biradar <ramlingbiradar64@gmail.com>
2024-03-18 16:34:23 +0530

    initial Commit.

---
+++ Capgemini/src/ShapeDemo.java
@@ -0,0 +1,15 @@
+
+public class ShapeDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Shape s=new Shape();
+		
+		System.out.println("Length :"+s.length);
+		System.out.println("Breadth is :"+s.breadth);
+		s.area();
+		
+
+	}
+
+}
